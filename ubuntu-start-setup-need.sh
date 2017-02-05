ddef0db5f449ae9fc1085d128cac5ea44acb3be1
zhujiagang <1058776021@qq.com>
2017-02-05 11:29:00 +0800

    2.5

--- ubuntu-start-setup-need.sh
+++ ubuntu-start-setup-need.sh
@@ -25,4 +25,5 @@ sudo apt-get -y install libqt4-dev libgtk2.0-dev
 sudo apt-get -y install libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev
 sudo apt-get -y install x264 v4l-utils ffmpeg
 sudo apt-get -y install libgtk2.0-dev
+sudo apt-get -y install gfortran swig
 
